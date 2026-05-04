6ac5f9db6291443f260f8a25aaf8c8ddef53588e
RebelMythik <50276986+RebelMythik@users.noreply.github.com>
2026-05-04 12:54:53 -0400

    misc wrk

--- src/main/java/rebelmythik/rebelsTPA/commands/tpa/TpaHereCommand.java
+++ src/main/java/rebelmythik/rebelsTPA/commands/tpa/TpaHereCommand.java
@@ -1,4 +1,19 @@
 package rebelmythik.rebelsTPA.commands.tpa;
 
-public class TpaHereCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import rebelmythik.rebelsTPA.RebelsTPA;
+
+public class TpaHereCommand implements CommandExecutor {
+    RebelsTPA rebelsTPA;
+    public TpaHereCommand(RebelsTPA plugin) {
+        rebelsTPA = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        return false;
+    }
 }
--- src/main/java/rebelmythik/rebelsTPA/events/JoinEvent.java
+++ src/main/java/rebelmythik/rebelsTPA/events/JoinEvent.java
@@ -1,5 +1,6 @@
 package rebelmythik.rebelsTPA.events;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
@@ -7,6 +8,8 @@ public class JoinEvent implements Listener {
 
 
     public void playerJoin(PlayerJoinEvent event) {
+        String player = String.valueOf(event.getPlayer().getUniqueId());
+
         // Logic to Check db and Create Entry if they don't exist
     }
 }
